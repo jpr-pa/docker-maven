43aa533d784f602666a82dab213f966033f9b0f0
Jai <jaipalreddypothireddy23@gmail.com>
2025-05-08 11:26:16 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
 
+@RestController
+class MessageController {
+
+    @GetMapping("/")
+    public String showMessage() {
+        return "This is Jaipal, presently working at Nexnoratech";
+    }
 }
